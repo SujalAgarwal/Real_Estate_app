3f443394495acb62af62110f7712141168b6afbe
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-03-21 14:27:02 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+mongoose.connect(process.env.MONGO_URl).then(()=>{
+  console.log("Database connected")}).catch((e)=>{
+    console.log(e.message)
+  })
+
 const app=express();
 app.listen(3000,()=>{
   console.log("server stsrted on port 3000 !!!");
