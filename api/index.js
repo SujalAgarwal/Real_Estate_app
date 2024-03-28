9016239354c2dbdabe4a390364d064f1ba9af409
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-03-28 14:15:17 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -1,31 +1,43 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import authRouter from "./routes/authRoutes.js";
-import userRouter from "./routes/userRoutes.js"
-import listingRouter from "./routes/listingRoute.js"
+import userRouter from "./routes/userRoutes.js";
+import listingRouter from "./routes/listingRoute.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
-mongoose.connect(process.env.MONGO_URl).then(()=>{
-  console.log("Database connected")}).catch((e)=>{
-    console.log(e.message)
+mongoose
+  .connect(process.env.MONGO_URl)
+  .then(() => {
+    console.log("Database connected");
   })
+  .catch((e) => {
+    console.log(e.message);
+  });
 
-const app=express();
-app.use(express.json())
-app.use(cookieParser())
-app.listen(3000,()=>{
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.listen(3000, () => {
   console.log("server started on port 3000 !!!");
-})
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
-app.use('/api/listing',listingRouter)
-app.use((err,req,res,next)=>{
-  const statuscode=err.statuscode||500;
-  const message=err.message||"internal server error";
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+app.use((err, req, res, next) => {
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "internal server error";
   return res.status(500).json({
-    success:false,
+    success: false,
     statuscode,
-    message
-  })
-})
+    message,
+  });
+});
