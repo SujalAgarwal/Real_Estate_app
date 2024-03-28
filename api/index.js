6d322e47daa820a756cc48ae70d1fec2f75be989
Sujal Agarwal <agarwalsujal211@gmail.com>
2024-03-28 09:10:04 +0530

    completed update user functionality

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
-import userRouter from "./routes/authRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js"
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose.connect(process.env.MONGO_URl).then(()=>{
   console.log("Database connected")}).catch((e)=>{
@@ -10,11 +12,12 @@ mongoose.connect(process.env.MONGO_URl).then(()=>{
 
 const app=express();
 app.use(express.json())
+app.use(cookieParser())
 app.listen(3000,()=>{
   console.log("server started on port 3000 !!!");
 })
-
-app.use('/api/auth',userRouter)
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
 
 app.use((err,req,res,next)=>{
   const statuscode=err.statuscode||500;
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -6,25 +6,61 @@ const initialState={
   loading:false
 };
 
-const userSlice=createSlice({
-  name:'user',
+const userSlice = createSlice({
+  name: "user",
   initialState,
-  reducers:{
-    signInStart:(state)=>{
-      state.loading=true;
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
     },
-    signInSuccess:(state,action)=>{
-      state.currentuser=action.payload;
-      state.loading=false;
-      state.error=null;
+    signInSuccess: (state, action) => {
+      state.currentuser = action.payload;
+      state.loading = false;
+      state.error = null;
     },
-    signInFaliure:(state,action)=>{
-      state.error=action.payload;
-      state.loading=false;
-    }
-  }
+    signInFaliure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentuser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentuser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentuser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
 });
 
-export const {signInFaliure,signInStart,signInSuccess}=userSlice.actions;
+export const {signInFaliure,signInStart,signInSuccess,updateUserFailure,updateUserStart,updateUserSuccess,deleteUserStart,deleteUserSuccess,deleteUserFailure,signOutUserStart,signOutUserSuccess,signOutUserFaliure}=userSlice.actions;
 
 export default userSlice.reducer;
